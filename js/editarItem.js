35efc78ab87bbcaaef4867a4c1c38588c79cec9d
Rodrigo Furstenau Cannabrava <rodrigofcannabrava@gmail.com>
2024-09-10 22:04:14 -0300

    projeto finalizado

---
+++ js/editarItem.js
@@ -0,0 +1,21 @@
+const editarItem = (elemento) => {
+    let novoItem = prompt("Digite o novo nome do item:");
+
+    if(novoItem !== null && novoItem.trim() !== "") {
+        const itemTextoAtualizado = elemento.querySelector("#item-titulo")
+        itemTextoAtualizado.textContent = novoItem;
+
+        const estavaComprado = elemento.querySelector(".input-checkbox").checked;
+
+        if(estavaComprado) {
+            elemento.querySelector(".input-checkbox").checked = true;
+            elemento.querySelector(".checkbox-customizado").classList.add("checked");
+            itemTextoAtualizado.style.textDecoration = "line-through";
+        }
+
+        const textoData = elemento.querySelector(".texto-data");
+        textoData.innerText = `${new Date().toLocaleDateString("pt-BR", { weekday: "long" })} (${new Date().toLocaleDateString()}) às ${new Date().toLocaleTimeString("pt-BR", { hour: "numeric", minute: "numeric" })}`;
+    }
+}
+
+export { editarItem };
---
+++ js/verificarListaComprados.js
@@ -0,0 +1,9 @@
+const containerListaComprados = document.getElementById("container-lista-comprados");
+
+export function verificarListaComprados(lista) {
+    if (lista.childElementCount === 0) {
+        containerListaComprados.style.display = "none"
+    } else {
+        containerListaComprados.style.display = "block"
+    }
+}
